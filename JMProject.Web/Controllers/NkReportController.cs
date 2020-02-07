566f77c398aa59d61ac025be990bdd325f355f88
jxj050928 <110159906@qq.com>
2020-02-07 10:19:30 +0800

    内控报告控制器

---
+++ JMProject.Web/Controllers/NkReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JMProject.Web.Controllers
+{
+    public class NkReportController : Controller
+    {
+        //
+        // GET: /NkReport/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
